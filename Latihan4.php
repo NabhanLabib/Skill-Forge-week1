6f46b8cba21554ecbfa91de71379712fb2dbc80c
NabhanLabib <Nabhanlab@gmail.com>
2024-04-28 23:06:00 +0700

    update

---
+++ Latihan4.php
@@ -0,0 +1,15 @@
+<?php
+//Variabel tetap
+$A=99;
+
+//Nilai operasi
+$NilaiOperasi=3;
+$NilaiPembagi=9;
+
+
+$HasilOperasi= $A*$NilaiOperasi+$NilaiOperasi-$NilaiOperasi;
+
+
+//Tampilan hasil
+echo "Nilai A ketika dikali, Ditambah, dan Dikurangi Nilai Operasi maka hasilnya adalah:" .$HasilOperasi;
+echo "\n Hasil Operasi dibagi nilai pembagi:" .$Hasilbagi=$HasilOperasi/$NilaiPembagi;
